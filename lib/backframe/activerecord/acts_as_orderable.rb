3aba22dbbbf26ad159c265b28e61763ee2702159
Jared Beck <jared@jaredbeck.com>
2016-09-26 15:01:42 -0400

    acts_as_orderable: Crash earlier if config invalid

--- lib/backframe/activerecord/acts_as_orderable.rb
+++ lib/backframe/activerecord/acts_as_orderable.rb
@@ -9,9 +9,10 @@ module Backframe
     end
 
     module ClassMethods
-      def acts_as_orderable(field, args = {})
+      def acts_as_orderable(field, on)
         class_eval <<-EOV, __FILE__, __LINE__ + 1
           validates_presence_of :#{field}
+          validates_presence_of :#{on}
 
           before_validation :set_#{field}, :on => :create
           after_destroy :reorder_#{field}
@@ -19,11 +20,11 @@ module Backframe
           default_scope -> { order(:#{field} => :asc) }
 
           def set_#{field}
-            self.#{field} ||= (self.#{args[:on]}.#{self.name.tableize}.any?) ? self.#{args[:on]}.#{self.name.tableize}.maximum(:#{field}) + 1 : 0
+            self.#{field} ||= (self.#{on}.#{self.name.tableize}.any?) ? self.#{on}.#{self.name.tableize}.maximum(:#{field}) + 1 : 0
           end
 
           def reorder_#{field}
-            self.#{args[:on]}.#{self.name.tableize}.order(:#{field} => :asc).each_with_index do |item, index|
+            self.#{on}.#{self.name.tableize}.order(:#{field} => :asc).each_with_index do |item, index|
               item.update_column(:#{field}, index)
             end
           end
