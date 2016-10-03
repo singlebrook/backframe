9a63e3dacf0f933c4e6d7e162c2f5de1cb4c9995
Jared Beck <jared@jaredbeck.com>
2016-10-03 13:01:35 -0400

    Code style

--- lib/backframe/models/reset.rb
+++ lib/backframe/models/reset.rb
@@ -31,29 +31,28 @@ module Backframe
 
     private
 
-      def init
-        self.token ||= SecureRandom.hex(32).to_s.upcase[0,32]
-        self.is_active = true
-      end
+    def init
+      self.token ||= SecureRandom.hex(32).to_s.upcase[0,32]
+      self.is_active = true
+    end
 
-      def set_active
-        self.is_active = true
-      end
+    def set_active
+      self.is_active = true
+    end
 
-      def enforce_uniqueness
-        self.user.resets.where('id != ?', self.id).update_all(:is_active => false)
-      end
+    def enforce_uniqueness
+      self.user.resets.where('id != ?', self.id).update_all(:is_active => false)
+    end
 
-      def send_email
-        if self.user_type == 'Admin'
-          ApplicationMailer.reset(self).deliver_now!
-        elsif self.user_type == 'Customer'
-          template = EmailTemplate.find_by(:code => 'password_reset')
-          email = EmailDelivery.new(:customer => self.user, :subject => template.subject, :body => template.body)
-          email.personalize(:customer => self.user, :reset => self)
-          email.save
-        end
+    def send_email
+      if self.user_type == 'Admin'
+        ApplicationMailer.reset(self).deliver_now!
+      elsif self.user_type == 'Customer'
+        template = EmailTemplate.find_by(:code => 'password_reset')
+        email = EmailDelivery.new(:customer => self.user, :subject => template.subject, :body => template.body)
+        email.personalize(:customer => self.user, :reset => self)
+        email.save
       end
-
+    end
   end
 end
